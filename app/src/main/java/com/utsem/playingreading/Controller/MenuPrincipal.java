6fb9ba9a725abb51d932893b91c6e3343318d43d
Santiago <santiagojesuslaureano369@gmail.com>
2025-02-02 20:41:23 -0600

    Se avanza en el menu pricipal, aun faltan cambios

--- app/src/main/java/com/utsem/playingreading/Controller/MenuPrincipal.java
+++ app/src/main/java/com/utsem/playingreading/Controller/MenuPrincipal.java
@@ -1,8 +1,6 @@
 package com.utsem.playingreading.Controller;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 //bluetooth
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -13,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -22,13 +19,10 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Set;
 import com.utsem.playingreading.R;
 import com.utsem.playingreading.Services.BluetoothService;
-
 public class MenuPrincipal extends AppCompatActivity {
     private static final String TAG = "MenuPrincipal";
     private BluetoothService bluetoothService;
     private boolean isBound = false;
-    LinearLayout contenido, noHayMaquina;
-    Button btnNoMaquina;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,25 +34,22 @@ public class MenuPrincipal extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button connectButton = findViewById(R.id.btnIniciar);
+        Button connectButton = findViewById(R.id.btnConectar);
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-/*
-        //ocultar la pagina de botones
-        contenido = (LinearLayout) findViewById(R.id.lyContenidoBotones);
-        contenido.setVisibility(View.GONE);
-        noHayMaquina = (LinearLayout) findViewById(R.id.lyNoHayMaquina);
-        noHayMaquina.setVisibility(View.VISIBLE);
-        btnNoMaquina = (Button) findViewById(R.id.btnNoHayMaquina);
-*/
+        LinearLayout vistaConectar =findViewById(R.id.lnyConectar);
+        LinearLayout vistaContenido =findViewById(R.id.lnyContenido);
+        LinearLayout vistaBarra =findViewById(R.id.lnyBarra);
 
+
+        vistaConectar.setVisibility(View.VISIBLE);
+        vistaContenido.setVisibility(View.GONE);
+        vistaBarra.setVisibility(View.GONE);
 
         // Vincula el servicio Bluetooth
         Intent intent = new Intent(this, BluetoothService.class);
         bindService(intent, serviceConnection, BIND_AUTO_CREATE);
-
         connectButton.setOnClickListener(v -> {
-
             if (bluetoothAdapter == null) {
                 connectButton.setText("IMPOSIBLE CONECTAR");
                 Toast.makeText(this, "Bluetooth no está disponible", Toast.LENGTH_SHORT).show();
@@ -67,11 +58,21 @@ public class MenuPrincipal extends AppCompatActivity {
             if (!bluetoothAdapter.isEnabled()) {
                 connectButton.setText("CONECTAR");
                 Toast.makeText(this, "Activa el Bluetooth primero", Toast.LENGTH_SHORT).show();
+
+
+
+                //poner para que se muestre solo cuando se conecte a la maquina
+                //por lo mientras solo se muestra en pruebas
+                vistaBarra.setVisibility(View.VISIBLE);
+                vistaConectar.setVisibility(View.GONE);
+                vistaContenido.setVisibility(View.VISIBLE);
+                //aqui termina
+
+
+
                 return;
             }
-
             Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-
             if (pairedDevices.size() > 0) {
                 BluetoothDevice device = pairedDevices.iterator().next(); // Selecciona el primer dispositivo
                 boolean success = bluetoothService.connectToDevice(device);
@@ -81,19 +82,29 @@ public class MenuPrincipal extends AppCompatActivity {
                     if(connectButton.getText().toString().equals("CONECTADO")){
                         bluetoothService.startListening();
                     }
-
                 } else {
                     connectButton.setText("DESCONECTADO");
                     Toast.makeText(this, "Error al conectar", Toast.LENGTH_SHORT).show();
                 }
             } else {
-                Toast.makeText(this, "No hay dispositivos emparejados", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "No hay dispositivos emparejados",   Toast.LENGTH_SHORT).show();
+
+                //poner para que se muestre solo cuando se conecte a la maquina
+                //por lo mientras solo se muestra en pruebas
+                vistaBarra.setVisibility(View.VISIBLE);
+                vistaConectar.setVisibility(View.GONE);
+                vistaContenido.setVisibility(View.VISIBLE);
+                //aqui termina
+
+
             }
         });
+
     }
 
-    // Maneja la conexión al servicio
 
+
+    // Maneja la conexión al servicio
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -108,19 +119,16 @@ public class MenuPrincipal extends AppCompatActivity {
         }
     };
 
+    public void salir(View v){
+        this.finish();
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (isBound) {
-            //     unbindService(serviceConnection);
+            //unbindService(serviceConnection);
             isBound = false;
         }
     }
-
-    //no hay maquina
-    public void noHayMaquina(View v){
-        noHayMaquina.setVisibility(View.GONE);
-        contenido.setVisibility(View.VISIBLE);
-    }
-//Comentario
 }
